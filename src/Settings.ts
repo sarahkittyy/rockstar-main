f3f687119c8f9be8ff470a9f999a516a6ac97a39
sarahkittyy <sarahohlin1@gmail.com>
2019-07-01 06:46:38 -0400

    added path and server ports to settings

--- src/Settings.ts
+++ src/Settings.ts
@@ -2,4 +2,6 @@ export default interface Settings
 {
 	cmdprefix: string; // The prefix to use before commands
 	cmdnotifybad: boolean; // Whether or not to notify if a command isn't valid.
+	serverpath: string; // The base path to the port-indexed servers.
+	ports: Array<number>; // All the SCP Server ports to log events from.
 };
